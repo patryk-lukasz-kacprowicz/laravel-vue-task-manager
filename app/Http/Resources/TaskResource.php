168fda137f7d0e847b4a32947bb47118653e1998
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-09-20 03:45:13 +0200

    refactor: add display task id to task resource

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -15,6 +15,7 @@ class TaskResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'is_completed' => $this->is_completed,
             'title' => $this->title,
             'description' => $this->description,
