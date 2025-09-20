b23a31937d9ceb93086abf2c5a79b25ec6fbbf0a
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-09-20 02:53:16 +0200

    feat: create resource for get task data

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'is_completed' => $this->is_completed,
+            'title' => $this->title,
+            'description' => $this->description,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
