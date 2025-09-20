760bf1588adbb8ee6726565a3e3681b32d2762ba
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-09-20 03:45:26 +0200

    feat: add route for display list of tasks

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -25,3 +26,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
