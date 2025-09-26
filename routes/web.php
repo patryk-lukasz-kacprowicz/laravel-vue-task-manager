6432b6a84e367d2f78eea0d4491503ab08547f50
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-09-26 04:54:43 +0200

    feat: create routes for task CRUD

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,9 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('tasks', [TaskController::class, 'store'])->name('tasks.store');
+Route::get('tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
+Route::get('tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::patch('tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
