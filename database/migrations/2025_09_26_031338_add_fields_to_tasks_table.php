f03886030e070184b8bb3d20961f511b64451c96
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-09-26 18:42:05 +0200

    feat: create migration for add deadline_date and completion_date fields to tasks table

---
+++ database/migrations/2025_09_26_031338_add_fields_to_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->timestamp('completion_date')->nullable()->default(null)->after('description');
+            $table->date('deadline_date')->nullable()->default(null)->after('completion_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('completion_date');
+            $table->dropColumn('deadline_date');
+        });
+    }
+};
