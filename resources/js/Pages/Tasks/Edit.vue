65f6627fdac58695bf5beb4bdee875072c7b21d4
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-09-26 19:40:51 +0200

    refactor: change display title page text from left to center page

--- resources/js/Pages/Tasks/Edit.vue
+++ resources/js/Pages/Tasks/Edit.vue
@@ -72,51 +72,59 @@ const submitForm = async () => {
 
     <Navbar />
 
-    <div class="dark-bg">
-        <div class="bg-black bg-opacity-85 flex-grow">
-            <div class="container mx-auto p-4">
-                <div class="mb-6 mt-6">
-                    <h3 class="text-3xl font-bold text-gray-200">
-                        Edit a task
-                    </h3>
-                </div>
-                <div class="p-6 bg-neutral-900 rounded-lg shadow-md mt-4 mb-2">
-                    <div class="mb-3" v-if="statusMessage.message">
-                        <div v-if="statusMessage.message" class="mt-4 p-4 rounded" :class="{
-                            'bg-green-500 text-white': statusMessage.type === 'success',
-                            'bg-red-500 text-white': statusMessage.type === 'error',
-                            'bg-blue-500 text-white': statusMessage.type === 'info',
-                        }">
-                            {{ statusMessage.message }}
-                        </div>
+    <div class="dark-bg min-h-screen">
+        <div class="bg-black bg-opacity-85 flex-grow py-8">
+            <div class="container mx-auto p-4 sm:p-6 lg:p-8">
+                <div class="max-w-xl lg:max-w-3xl mx-auto">
+                    <div class="mb-6 mt-6">
+                        <h3 class="text-3xl font-bold text-gray-200">
+                            Edit Task
+                        </h3>
                     </div>
 
-                    <div class="space-y-2">
-                        <form @submit.prevent="submitForm">
-                            <div class="grid gap-6 mb-6 md:grid-cols-1">
-                                <div>
-                                    <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task title</label>
-                                    <input type="text" id="title" v-model="form.title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Create app" required />
-                                    <p v-if="validationErrors.title" class="mt-2 text-sm text-red-500">{{ validationErrors.title }}</p>
-                                </div>
+                    <div class="p-6 bg-neutral-900 rounded-lg shadow-2xl mt-4 mb-2">
+                        <div class="mb-3" v-if="statusMessage.message">
+                            <div class="p-4 rounded" :class="{
+                                'bg-green-500 text-white': statusMessage.type === 'success',
+                                'bg-red-500 text-white': statusMessage.type === 'error',
+                                'bg-blue-500 text-white': statusMessage.type === 'info',
+                            }">
+                                {{ statusMessage.message }}
+                            </div>
+                        </div>
 
-                                <div>
-                                    <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task description</label>
-                                    <textarea id="description" v-model="form.description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Application must be well made!" required />
-                                    <p v-if="validationErrors.description" class="mt-2 text-sm text-red-500">{{ validationErrors.description }}</p>
-                                </div>
-
-                                <div class="flex items-start mb-6">
-                                    <div class="flex items-center h-5">
-                                        <input
-                                            id="is_completed" v-model="form.is_completed" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" />
-                                    </div>
-                                    <label for="is_completed" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Is task complete?</label>
-                                    <p v-if="validationErrors.is_completed" class="mt-2 text-sm text-red-500">{{ validationErrors.is_completed }}</p>
-                                </div>
+                        <form @submit.prevent="submitForm" class="space-y-6">
+                            <div>
+                                <label for="title" class="block mb-2 text-sm font-medium text-gray-200">Task title</label>
+                                <input type="text" id="title" v-model="form.title" class="form-input block w-full bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 rounded-lg" placeholder="Update app" required />
+                                <p v-if="validationErrors.title" class="mt-2 text-sm text-red-500">{{ validationErrors.title }}</p>
                             </div>
 
-                            <button type="submit" class="p-2 text-white bg-blue-800 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create task</button>
+                            <div>
+                                <label for="description" class="block mb-2 text-sm font-medium text-gray-200">Task description</label>
+                                <textarea id="description" v-model="form.description" rows="4" class="form-textarea block w-full bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 rounded-lg" placeholder="Application details..." />
+                                <p v-if="validationErrors.description" class="mt-2 text-sm text-red-500">{{ validationErrors.description }}</p>
+                            </div>
+
+                            <div class="grid gap-6 md:grid-cols-2">
+
+                                <div>
+                                    <label for="deadline_date" class="block mb-2 text-sm font-medium text-gray-200">Deadline date</label>
+                                    <input type="date" id="deadline_date" v-model="form.deadline_date" class="form-input block w-full bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 rounded-lg" />
+                                    <p v-if="validationErrors.deadline_date" class="mt-2 text-sm text-red-500">{{ validationErrors.deadline_date }}</p>
+                                </div>
+
+                            </div>
+
+                            <div class="flex items-center pt-2">
+                                <input id="is_completed" v-model="form.is_completed" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-700 border-gray-600 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2" />
+                                <label for="is_completed" class="ms-2 text-sm font-medium text-gray-300">Is task complete?</label>
+                                <p v-if="validationErrors.is_completed" class="mt-2 text-sm text-red-500">{{ validationErrors.is_completed }}</p>
+                            </div>
+
+                            <button type="submit" :disabled="form.processing" class="w-full sm:w-auto px-5 py-2.5 text-center text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm transition duration-150 ease-in-out disabled:opacity-50">
+                                Update task
+                            </button>
                         </form>
                     </div>
                 </div>
