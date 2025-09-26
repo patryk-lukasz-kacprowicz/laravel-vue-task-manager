1811603fcf73ef46dd065369c8948545bd7af415
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-09-26 19:11:50 +0200

    refactor: add navbar component to create task page

--- resources/js/Pages/Tasks/Create.vue
+++ resources/js/Pages/Tasks/Create.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
 import { ref } from 'vue';
+import Navbar from "@/Components/Custom/Navbar.vue";
 
 const validationErrors = ref({});
 const statusMessage = ref({ message: '', type: '' });
@@ -62,6 +63,8 @@ const submitForm = async () => {
 
 <template>
     <Head title="TODOapp - Create task" />
+
+    <Navbar />
 
     <div class="dark-bg">
         <div class="bg-black bg-opacity-85 flex-grow">
