1d5b1c37d75dfb39c7e5972e1d7ab9973ed55d24
Patryk Kacprowicz <patryk.l.kacprowicz@gmail.com>
2025-09-27 02:50:23 +0200

    feat: add scopes for get task data

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
 
 class Task extends Model
 {
@@ -24,6 +25,46 @@ class Task extends Model
         'deadline_date' => 'datetime',
         'completion_date' => 'datetime',
     ];
+
+    /**
+     * @param Builder $query
+     * @param $status
+     *
+     * @return void
+     */
+    public function scopeFilterByStatus(Builder $query, $status = null): void {
+        if (strlen($status) > 0 || $status === 0 || $status === '0') {
+            $query->where('is_completed', (bool) $status);
+        }
+    }
+
+    /**
+     * @param Builder $query
+     * @param $priority
+     *
+     * @return void
+     */
+    public function scopeFilterByPriority(Builder $query, $priority = null): void {
+        if ($priority) {
+            $query->where('priority', $priority);
+        }
+    }
+
+    /**
+     * @param Builder $query
+     * @param $column
+     * @param $direction
+     *
+     * @return void
+     */
+    public function scopeApplySort(Builder $query, $column = 'created_at', $direction = 'desc'): void {
+        $validColumns = ['created_at', 'deadline_date', 'priority', 'title'];
+        $validDirections = ['asc', 'desc'];
+
+        if (in_array($column, $validColumns) && in_array($direction, $validDirections)) {
+            $query->orderBy($column, $direction);
+        }
+    }
 
     /**
      * @return BelongsTo
